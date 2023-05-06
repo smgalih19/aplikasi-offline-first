81e066997c0e47bd99addca4a25112b16f6e938c
Sugih Mohammad Galih <59138555+smgalih19@users.noreply.github.com>
2023-05-06 21:17:17 +0700

    push kahiji

---
+++ app/src/main/java/com/example/functionalcompose/persistences/SetoranSampahDao.kt
@@ -0,0 +1,26 @@
+package com.example.functionalcompose.persistences
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.functionalcompose.model.SetoranSampah
+
+@Dao
+interface SetoranSampahDao {
+    @Query("SELECT * FROM SetoranSampah ORDER BY tanggal DESC")
+    fun loadAll(): LiveData<List<SetoranSampah>>
+
+    @Query("SELECT * FROM SetoranSampah ORDER BY tanggal DESC")
+    suspend fun getList(): List<SetoranSampah>
+
+    @Query("SELECT * FROM SetoranSampah WHERE id = :id")
+    fun find(id: String): SetoranSampah?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(vararg items: List<SetoranSampah>)
+
+    @Delete
+    fun delete(item: SetoranSampah)
+
+    @Query("DELETE FROM SetoranSampah WHERE id = :id")
+    fun delete(id: String)
+}
